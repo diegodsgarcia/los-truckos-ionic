825eb1ed0e28073699960e942600ef5368d77cb2
logonrm <logonrm@fiap.com.br>
2018-05-16 19:57:27 -0300

    Criação do projeto e implementação do angularfire2

---
+++ src/environments/environment.model.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBsPZBgqFQ8pv4OHNt778nHEuw8RQkbqbM",
+  authDomain: "lostruckos.firebaseapp.com",
+  databaseURL: "https://lostruckos.firebaseio.com",
+  projectId: "lostruckos",
+  storageBucket: "lostruckos.appspot.com",
+  messagingSenderId: "552193187403",
+};
