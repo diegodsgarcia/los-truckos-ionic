2679105bc9ecce4ba28622bca1de175e4f997750
logonrm <logonrm@fiap.com.br>
2018-05-16 22:35:29 -0300

    Imlementação da page logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,20 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { firebaseConfig } from '../environments/environment';
 
+// Pages
 import { MyApp } from './app.component';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 import { MapPage } from '../pages/map/map';
 import { SettingsPage } from '../pages/settings/settings';
+
+// Providers
+import { UserProvider } from '../providers/user';
 
 @NgModule({
   declarations: [
     MyApp,
+    LoginPage,
     TabsPage,
     MapPage,
     SettingsPage,
@@ -33,13 +39,15 @@ import { SettingsPage } from '../pages/settings/settings';
   entryComponents: [
     MyApp,
     TabsPage,
+    LoginPage,
     MapPage,
     SettingsPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserProvider
   ]
 })
 export class AppModule {}
