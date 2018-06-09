9e253ff6f07c338394836b84ff91cb0592535842
diegodsgarcia <diego.ds.garcia@gmail.com>
2018-06-09 19:28:13 -0300

    Implementacao bas do login e do metodo de login pelo gmail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
 
@@ -12,10 +14,27 @@ import { LoginPage } from '../pages/login/login';
 export class MyApp {
   rootPage: any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    ) {
+
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
+    });
+
+  }
+
+  checkIfIsAuthenticate() {
+    this.afAuth.authState.subscribe((isAuth) => {
+      if(isAuth) {
+        this.rootPage = TabsPage;
+      } else {
+        this.rootPage = LoginPage;
+      }
     });
   }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,21 +1,42 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
+
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    private afAuth: AngularFireAuth,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+  ) {}
 
   ionViewDidLoad() {
 
   }
 
-  login(typeOfLogin: string) {
+  login() {
+    // this.signInWithGoogle().then((datas) => {
+    //   console.log(datas);
+    // }).catch((error) => {
+    //   console.log(error);
+    // })
+  }
 
+  signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    if (!(<any>window).cordova) {
+      return this.afAuth.auth.signInWithPopup(provider);
+    } else {
+      return this.afAuth.auth.signInWithRedirect(provider)
+        .then(() => this.afAuth.auth.getRedirectResult());
+    }
   }
 
 }
