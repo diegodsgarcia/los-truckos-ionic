9fe29f527c5541ca128615db1888e91dc0170753
diegodsgarcia <diego.ds.garcia@gmail.com>
2018-06-10 16:45:27 -0300

    Criacao da animacao do searchbar

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef } from '@angular/core'
 import { NavController } from 'ionic-angular'
 import { Foodtruck, Location } from '../../models/index'
 
@@ -8,11 +8,15 @@ import { Foodtruck, Location } from '../../models/index'
 })
 export class MapPage {
   foodtrucks: Foodtruck[]
-  latitude = 51.678418
-  longitude = 7.809007
+  latitude: number = 51.678418
+  longitude: number = 7.809007
+  list: HTMLElement
+  searchbar: HTMLElement
 
 
-  constructor(public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController, public elementRef: ElementRef) {
+
     const location = new Location(1, 1, '7º Andar, Av. Paulista, 1106 - Bela Vista, São Paulo - SP, 01311-000')
     this.foodtrucks = [
       new Foodtruck(new Date().getTime(), "Lorem", "lorem@gmail.com", "img.png", location),
@@ -22,6 +26,22 @@ export class MapPage {
   }
 
   ionViewDidLoad() {
+    this.list = this.elementRef.nativeElement.querySelector('.list')
+    this.searchbar = this.elementRef.nativeElement.querySelector('.searchbar')
+  }
+
+  openList() {
+    if (!this.list.classList.contains('list--active')) {
+      this.list.classList.add('list--active')
+      this.searchbar.classList.add('searchbar--active')
+    }
+  }
+
+  closeList() {
+    if (this.list.classList.contains('list--active')) {
+      this.list.classList.remove('list--active')
+      this.searchbar.classList.remove('searchbar--active')
+    }
   }
 
   onChoseLocation(event) {
