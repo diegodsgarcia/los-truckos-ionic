66878a51a185ba40ff8b99887bc0ff98ecc9ddeb
Diego Garcia <diego.ds.garcia@gmail.com>
2018-06-14 23:59:04 -0300

    Adição da do getCurrentGeolocation do usuario

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core'
 import { NavController, NavParams } from 'ionic-angular'
-import { User } from '../../models'
+import { User, Location } from '../../models'
 
+import { UserProvider } from '../../providers/user'
 import { AngularFireAuth } from 'angularfire2/auth'
 
 @Component({
@@ -13,17 +14,21 @@ export class SettingsPage {
   private user: User
   constructor(
     private afAuth: AngularFireAuth,
+    private userProvider: UserProvider,
     public navCtrl: NavController,
     public navParams: NavParams) {
 
-    this.user = new User(
-      'Diego Garcia',
-      'diego.ds.garcia@gmail.com', 'https://ionicframework.com/img/meta/ionic-framework-og.png'
-    )
+
   }
 
   ionViewDidLoad() {
-
+    this.userProvider.getLocation().then(location => {
+      this.user = new User(
+        'Diego Garcia',
+        'diego.ds.garcia@gmail.com',
+        'https://ionicframework.com/img/meta/ionic-framework-og.png',
+      )
+    })
   }
 
   logout() {
--- src/providers/user.ts
+++ src/providers/user.ts
@@ -1,12 +1,26 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Geolocation } from '@ionic-native/geolocation'
+
+import { Location } from '../models'
 
 
 @Injectable()
 export class UserProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello UserProvider Provider');
+  constructor(
+    public http: HttpClient,
+    public geolocation: Geolocation
+  ) {
+
+  }
+
+  getLocation() {
+    return new Promise((resolve, reject) => {
+      this.geolocation.getCurrentPosition().then((pos) => {
+        resolve(new Location(pos.coords.latitude, pos.coords.longitude, '1'))
+      })
+    })
   }
 
 }
