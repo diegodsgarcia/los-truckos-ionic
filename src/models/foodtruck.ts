0da3a54fd208db1bc8d902abae54b4f7075275a2
Diego Garcia <diego.ds.garcia@gmail.com>
2018-06-14 22:44:30 -0300

    Adição de novas propriedades do model Foodtruck

--- src/models/foodtruck.ts
+++ src/models/foodtruck.ts
@@ -2,10 +2,15 @@ import { Location } from './index'
 
 export class Foodtruck {
   constructor(
-    public id: number,
     public name: string,
-    public email: string,
-    public logo?: string,
+    public owner: string,
+    public isOpen: string,
+    public speciality: string,
+    public logo: string,
+    public phone?: string,
+    public email?: string,
+    public facebook?: string,
+    public instagram?: string,
     public location?: Location,
   ) {}
 }
