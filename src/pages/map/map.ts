ea6caeb95f99960227a014e37c52e71be3cab64f
diegodsgarcia <diego.ds.garcia@gmail.com>
2018-06-16 02:45:15 -0300

    Atualizacao no rxjs, e implementacao do firebase na lista de foodtrucks

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,34 +1,56 @@
 import { Component, ElementRef } from '@angular/core'
 import { NavController } from 'ionic-angular'
+import { AngularFireDatabase } from 'angularfire2/database'
+
 import { Foodtruck, Location } from '../../models/index'
-import { DescriptionPage } from '../description/description';
+import { DescriptionPage } from '../description/description'
+
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html'
 })
 export class MapPage {
-  foodtrucks: Foodtruck[]
+  foodtrucks: Observable<Foodtruck[]>
   latitude: number = 51.678418
   longitude: number = 7.809007
   list: HTMLElement
   searchbar: HTMLElement
 
-  constructor(public navCtrl: NavController, public elementRef: ElementRef) {
+  constructor(
+    private db: AngularFireDatabase,
+    public navCtrl: NavController,
+    public elementRef: ElementRef) {
 
+    this.foodtrucks = this.getFoodtrucks()
+  }
+
+  private getFoodtrucks() {
     const location = new Location(51.678418, 7.809007, '7º Andar, Av. Paulista, 1106 - Bela Vista, São Paulo - SP, 01311-000')
-    this.foodtrucks = [new Foodtruck(
-      "Name",
-      "Owner",
-      false,
-      "Speciality",
-      "Logo",
-      location,
-      "Phone",
-      "Email",
-      "Facebook",
-      "Instagram"
-    )];
+
+    const datas: Observable<any[]> = this.db.list('foodtrucks').valueChanges()
+
+    datas.pipe(
+      map(foodtrucks =>
+        foodtrucks.map(foodtruck => new Foodtruck(
+          foodtruck.name,
+          foodtruck.owner,
+          foodtruck.isOpen,
+          foodtruck.speciality,
+          foodtruck.logo,
+          location,
+          foodtruck.phone,
+          foodtruck.email,
+          foodtruck.facebook,
+          foodtruck.instagram)
+        )
+      )
+    )
+
+    return datas
+
   }
 
   ionViewDidLoad() {
